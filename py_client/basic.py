78a74b1abc4974ad636333e32fc0b6c016506d06
Nicola Castaldo <nicola.castaldo@mail.polimi.it>
2022-06-25 09:36:33 +0200

    updates

--- py_client/basic.py
+++ py_client/basic.py
@@ -7,3 +7,26 @@ response = requests.get(endpoint, params={
                         'key-param': 'value-param'}, json={'data': 'some data in JSON format'})
 
 print(response.json(), response.status_code)
+
+
+class Point:
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+
+p = Point(2, 6)
+
+print(p.x)
+
+p._x = 4
+
+print(p.x)
